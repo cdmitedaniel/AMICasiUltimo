5524df8c9867bba879e296cca2abc22f9a483888
cdmitedaniel <cdmitedaniel@gmail.com>
2020-01-21 17:06:23 -0500

    Commit Data

---
+++ proyectoAmi-master/frontend/src/app/components/leyes/leyes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leyes',
+  templateUrl: './leyes.component.html',
+  styleUrls: ['./leyes.component.css']
+})
+export class LeyesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ proyectoAmi-master/frontend/src/app/components/votar/votar.component.ts
@@ -0,0 +1,127 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { LeyService } from '../../services/ley.service';
+import { Ley } from '../../models/ley';
+
+import { ArticuloService } from '../../services/articulo.service';
+import { Articulo } from '../../models/articulo';
+
+import { User } from "../../models/user";
+import { Router } from '@angular/router';
+
+
+declare var M: any;
+@Component({
+  selector: 'app-votar',
+  templateUrl: './votar.component.html',
+  styleUrls: ['./votar.component.css']
+
+})
+
+export class VotarComponent implements OnInit {
+
+  constructor(private leyService: LeyService, private articuloService: ArticuloService ) { }
+
+  ngOnInit() {
+    this.getLeyes();
+    this.getArticulos();
+  }
+
+  addLey(form?: NgForm) {
+    console.log(form.value);
+    if (form.value._id) {
+      this.leyService.putLey(form.value)
+        .subscribe(res => {
+          this.resetForm(form);
+          this.getLeyes();
+          M.toast({ html: 'ley actualizada' });
+        });
+    } else {
+      this.leyService.postLey(form.value)
+        .subscribe(res => {
+          this.getLeyes();
+          this.resetForm(form);
+          M.toast({ html: 'ley Guardado' });
+        });
+    }
+
+  }
+
+  getLeyes() {
+    this.leyService.getLeyes()
+    
+      .subscribe(res => {
+        this.leyService.leyes = res as Ley[];
+      
+      });
+  }
+  
+  editLey(ley: Ley) {
+    this.leyService.selectedLey = ley;
+  }
+
+  deleteLey(_id: string, form: NgForm) {
+    if (confirm('¿Esta seguro de eliminar?')) {
+      this.leyService.deleteLey(_id)
+        .subscribe(res => {
+          this.getLeyes();
+          this.resetForm(form);
+          M.toast({ html: 'Eiminado' });
+        });
+    }
+  }
+
+  resetForm(form?: NgForm) {
+    if (form) {
+      form.reset();
+      this.leyService.selectedLey = new Ley();
+    }
+  }
+
+
+  /////////Articulos/////////////////
+
+  addArticulo(form?: NgForm) {
+    console.log(form.value);
+    if(form.value._id) {
+      this.articuloService.putArticulo(form.value)
+        .subscribe(res => {
+          this.resetForm(form);
+          this.getArticulos;
+          M.toast({html: 'Articulo actualizado'});
+        });
+    } else {
+      this.articuloService.postArticulo(form.value)
+      .subscribe(res => {
+        this.getArticulos();
+        this.resetForm(form);
+        M.toast({html: 'articulo Guardado'});
+      });
+    }
+    
+  }
+
+  getArticulos() {
+    this.articuloService.getArticulos()
+      .subscribe(res => {
+        this.articuloService.articulos = res as Articulo[];
+      });
+  }
+
+  editArticulo(articulo: Articulo) {
+    this.articuloService.selectedArticulo = articulo;
+  }
+
+  deleteArticulo(_id: string, form: NgForm) {
+    if(confirm('¿Esta seguro de articulo?')) {
+      this.articuloService.deleteArticulo(_id)
+        .subscribe(res => {
+          this.getArticulos();
+          this.resetForm(form);
+          M.toast({html: 'Eiminado'});
+        });
+    }
+  }
+
+}
